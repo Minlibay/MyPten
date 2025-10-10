35508d831573d7dcaa4643b31652ca652e2303a2
HOLY ME <168169935+Minlibay@users.noreply.github.com>
2025-10-10 19:16:26 +0400

    Rename EnemyStats health cache

--- Scripts/Enemies/EnemyStats.cs
+++ Scripts/Enemies/EnemyStats.cs
@@ -1,20 +1,36 @@
 using UnityEngine;
 using Begin.Enemies;
 using Begin.Combat;
+using Begin.Core;
 
 namespace Begin.AI {
     [RequireComponent(typeof(Health))]
-    public class EnemyStats : MonoBehaviour {
+    public class EnemyStats : MonoBehaviour, IPooled {
         public EnemyDefinition def;
         public float moveSpeed;
         public float touchDamage;
+        public float touchInterval;
+
+        Health _cachedHealth;
 
         void Awake() {
+            _cachedHealth = GetComponent<Health>();
+            ApplyDefinition(true);
+        }
+
+        public void ApplyDefinition(bool refill) {
             if (!def) return;
             moveSpeed = def.moveSpeed;
             touchDamage = def.touchDamage;
-            var h = GetComponent<Health>();
-            h.max = def.maxHP; h.current = def.maxHP;
+            touchInterval = Mathf.Max(0.1f, def.touchInterval);
+            if (_cachedHealth) {
+                _cachedHealth.autoDestroy = false;
+                _cachedHealth.SetMax(def.maxHP, refill);
+            }
         }
+
+        public void OnSpawned() { ApplyDefinition(true); }
+
+        public void OnDespawned() { if (_cachedHealth) _cachedHealth.ResetState(true); }
     }
 }
